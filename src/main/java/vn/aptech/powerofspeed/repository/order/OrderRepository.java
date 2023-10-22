5b9d3c243c1d85b2e7d10de6cf826443aee4fd3d
duiqt <mrfuc@yahoo.com>
2023-10-22 19:29:56 +0700

    update

--- src/main/java/vn/aptech/powerofspeed/repository/order/OrderRepository.java
+++ src/main/java/vn/aptech/powerofspeed/repository/order/OrderRepository.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import vn.aptech.powerofspeed.model.order.Order;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends CrudRepository<Order, Long> {
 
     @Query(value = "select id from orders o order by o.id desc limit 1", nativeQuery = true)
     int getNextId();
