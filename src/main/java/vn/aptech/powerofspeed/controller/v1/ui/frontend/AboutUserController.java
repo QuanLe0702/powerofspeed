6b91fde1b55ec411aa72f6a6f49e634b31188c04
duiqt <mrfuc@yahoo.com>
2023-10-22 20:44:44 +0700

    update

--- src/main/java/vn/aptech/powerofspeed/controller/v1/ui/frontend/AboutUserController.java
+++ src/main/java/vn/aptech/powerofspeed/controller/v1/ui/frontend/AboutUserController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import vn.aptech.powerofspeed.repository.about.AboutRepository;
 import vn.aptech.powerofspeed.service.AboutService;
@@ -20,6 +21,11 @@ public class AboutUserController {
     public String about(Model model){
         model.addAttribute("abouts",aboutService.findAll());
         return "frontend/layout/pages/about";
+    }
+
+    @RequestMapping(value = {"/policy"}, method = RequestMethod.GET)
+    public String policy(){
+        return "frontend/layout/pages/policy";
     }
 //    @RequestMapping(value="about/{id}", method= RequestMethod.GET)
 //    public String about(Model model, @PathVariable("id") Long id){
